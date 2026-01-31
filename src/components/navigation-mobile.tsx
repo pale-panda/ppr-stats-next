b8cd7bed69bcdef0e5c5f4139529a4d6276ad19d
Alexander Torngren <alexander.torngren@drakeanalytics.se>
2026-01-31 02:16:30 +0100

    feat: improve mobile navigation and enhance link highlighting

--- src/components/navigation-mobile.tsx
+++ src/components/navigation-mobile.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Separator } from '@/components/ui/separator';
-import { type NavItems } from '@/lib/data/nav-links';
+import { NavItem, type NavItems } from '@/lib/data/nav-links';
 import { cn } from '@/lib/utils';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
@@ -20,31 +20,41 @@ export function NavigationMobile({
 }: NavigationMobileProps) {
   const pathname = usePathname();
 
+  const isCurrentPage = (link: NavItem) => {
+    if (link.altHref === undefined) {
+      return pathname.startsWith(link.href);
+    }
+
+    return (
+      pathname.startsWith(link.href) || pathname.startsWith(link.altHref || '')
+    );
+  };
+
   return (
     isNavOpen && (
       <nav
         role='navigation'
-        className='md:hidden backdrop-blur-md flex items-center bg-background/70 rounded-b-md shadow-md'>
-        <div className='backdrop-blur-md bg-background/70 rounded-b-md grid grid-cols-2 gap-0 w-full'>
+        className='md:hidden backdrop-blur-md bg-background/70 absolute top-16 w-dvw h-dvh max-h-dvh z-40 shadow-md'>
+        <div className='backdrop-blur-md bg-background/70 rounded-b-md gap-0 w-full h-full p-6'>
           {navLinks.map((link) => (
             <Link
               key={link.href}
               href={link.href}
               className={cn(
-                'group text-center block p-6 text-muted-foreground hover:text-foreground hover:bg-muted/50 transition-colors',
-                pathname.startsWith(link.href)
+                'group block text-xl text-muted-foreground hover:text-foreground hover:bg-muted/50 transition-colors',
+                isCurrentPage(link)
                   ? 'text-foreground'
-                  : 'text-muted-foreground'
+                  : 'text-muted-foreground',
               )}
-              aria-current={pathname.startsWith(link.href) ? 'page' : undefined}
+              aria-current={isCurrentPage(link) ? 'page' : undefined}
               title={link.title}
               onClick={() => setIsNavOpen(false)}>
               {link.label}
               <Separator
                 className={cn(
-                  'w-max mt-5 h-px opacity-0 transition-opacity group-hover:opacity-100 group-hover:bg-radial group-hover:from-primary/80 group-hover:to-transparent',
-                  pathname === link.href &&
-                    'opacity-100 bg-radial from-primary/80 to-transparent'
+                  'w-max mt-2 mb-5 h-px opacity-0 transition-opacity group-hover:opacity-100 group-hover:bg-radial group-hover:from-primary/80 group-hover:to-transparent',
+                  isCurrentPage(link) &&
+                    'opacity-100 bg-radial from-primary/80 to-transparent',
                 )}
               />
             </Link>
